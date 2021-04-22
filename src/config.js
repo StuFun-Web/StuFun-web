d1def2afe0c1f98b41c9493eb9c213ac016b19c1
tushargupta9412 <tushar.gla_cs18@gla.ac.in>
2021-04-22 09:57:24 +0530

    cofig

---
+++ src/config.js
@@ -0,0 +1,14 @@
+export default global.config = {
+    email: "",
+    classid : "",
+    classname : "",
+    classorg : "",
+    nav : "Classroom",
+    navcls : "Announcement",
+    user: "",
+    cemail:"",
+    chatsid:"",
+    chatrid:"",
+    chatrname:"",
+    // other global config variables you wish
+};
